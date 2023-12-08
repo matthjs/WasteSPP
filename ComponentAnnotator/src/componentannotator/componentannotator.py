bac7877035ddff88acbfd8c5205d76a2e745684a
Matthijs <matthijs.vanderlende@gmail.com>
2023-12-08 16:36:28 +0100

    setup class for later abandoned project file labelling using annotator (contributors: Matthijs, Andro)

--- ComponentAnnotator/src/componentannotator/componentannotator.py
+++ ComponentAnnotator/src/componentannotator/componentannotator.py
@@ -1,3 +1,4 @@
+from api.main import label_files
 from execution.annotation import AnnotationExecution
 from projectextractor.projectextractor import ProjectExtractor
 
@@ -5,7 +6,12 @@ from projectextractor.projectextractor import ProjectExtractor
 class ComponentAnnotator:
     def __init__(self):
         self.project_extractor = ProjectExtractor(min_stars=100, last_pushed_date="2022-01-01")
-        # self.annotator = AnnotationExecution()
 
     def _annotate_files(self):
-        self.project_extractor.find_abandoned_projects(10)
+        """
+
+        """
+        abandoned_projects = self.project_extractor.find_abandoned_projects(10)
+
+        for project in abandoned_projects:
+            label_files()
