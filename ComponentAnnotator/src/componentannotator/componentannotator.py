b7f7adcd73ec07d8d463181de3f11149a4b70e3d
Matthijs <matthijs.vanderlende@gmail.com>
2023-12-19 12:20:24 +0100

    fixed database issue and added a bit of documentation to component annotator [author: Matthijs, Andro]

--- ComponentAnnotator/src/componentannotator/componentannotator.py
+++ ComponentAnnotator/src/componentannotator/componentannotator.py
@@ -13,23 +13,41 @@ def get_label(distribution, taxonomy):
 
 
 class ComponentAnnotator:
+    """
+    The ComponentAnnotator class is responsible for annotating files in abandoned GitHub projects.
+    It utilizes the ProjectExtractor to find abandoned projects, the ComponentExtractor to run the
+    Arcan tool for component information, and the auto-fl annotator for file-level annotations (weak labels).
+    """
     def __init__(self):
+        """
+        Initializes the ComponentAnnotator with default values for the ProjectExtractor.
+        """
         self.project_extractor = ProjectExtractor(min_stars=100, last_pushed_date="2022-01-01")
         self.component_extractor = ComponentExtractor()
 
     def annotate_files(self):
         """
-        TODO: Use UI type call
+        Uses the project extractor to find abandoned github projects,
+        then runs the arcan tool (encapsulated in component_extractor) to get component information
+        and in after that runs the annotator (auto-fl) to get file level annotations (weak labels)
+        for all the files in the project.
         """
         abandoned_projects = self.project_extractor.find_abandoned_projects(10)
 
         for project in abandoned_projects:
             print(f"- {project['name']} ({project['html_url']})")
-            self.component_extractor.run_arcan(project['html_url'], "java")
-            # ret, _ = self._annotate_file(project['name'], project['html_url'], ["java"])
-            # print(ret)
+            # self.component_extractor.run_arcan(project['html_url'], "java")
+            ret, _ = self._annotate_file(project['name'], project['html_url'], ["java"])
+            print(ret)
 
     def _annotate_file(self, project_name: str, remote: str, languages: List[str]):
+        """
+        Request to auto-fl to annotate a Github project.
+        @param: project_name - name of the GitHub project.
+        @param: remote - HTML URL of the GitHub project.
+        @param: languages - list of programming languages of project
+        NOTE: Only Java project are fully supported and tested with the auto-fl annotator.
+        """
         url = 'http://auto-fl:8000/label/files'
         analysis = {
             "name": project_name,  # "Waikato|weka-3.8",
@@ -40,11 +58,11 @@ class ComponentAnnotator:
         res = requests.post(url, json=analysis)
         res = res.json()['result']
         taxonomy = res['taxonomy']
-        entries = []
+        file_entries = []
         files = res['versions'][0]['files']
         for file_name in files:
             file = files[file_name]
-            entries.append({
+            file_entries.append({
                 "path": file["path"],
                 "package": file["package"],
                 "distribution": file["annotation"]["distribution"],
@@ -52,6 +70,7 @@ class ComponentAnnotator:
                 "label": get_label(file["annotation"]["distribution"], taxonomy)
             })
 
-        annotations_df = pd.DataFrame(entries)
+        file_annot = pd.DataFrame(file_entries)
+        # Skip package and project leven annotations
 
-        return annotations_df, taxonomy
+        return file_annot, taxonomy
